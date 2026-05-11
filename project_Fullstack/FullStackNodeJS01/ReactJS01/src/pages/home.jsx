59af94d5404fbed6faf8eac57ac856df7f9b57b7
MaxTrann <tranlequocdai2005@gmail.com>
2026-05-11 15:28:38 +0700

    Bài tập 03 (11/05/2026) - mục 1 & 2

---
+++ project_Fullstack/FullStackNodeJS01/ReactJS01/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import { CrownTwoTone } from '@ant-design/icons';
+import { Result } from 'antd';
+
+const HomePage = () => {
+    return (
+        <div style={{ padding: 20 }}>
+            <Result
+                icon={<CrownTwoTone />}
+                title="JSON Web Token (React/Node.JS) - Trần Lê Quốc Đại"
+            />
+        </div>
+    )
+}
+
+export default HomePage;
